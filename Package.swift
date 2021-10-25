878222d6339b56d6a81bd83d5d1acdf0e2fe6d32
stefan draskovits <stefan.draskovits@allaboutapps.at>
2021-10-25 10:28:15 +0200

    add + fix tests

--- Package.swift
+++ Package.swift
@@ -1,19 +1,20 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "SimpleButton",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "SimpleButton", targets: ["SimpleButton"])
     ],
     dependencies: [
+        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing", .upToNextMajor(from: "1.9.0")),
     ],
     targets: [
-        .target(name: "SimpleButton", dependencies: [], path: "SimpleButton/")
-    ],
-    swiftLanguageVersions: [.v5]
+        .target(name: "SimpleButton", dependencies: [], path: "SimpleButton/"),
+        .testTarget(name: "SimpleButtonTests", dependencies: ["SimpleButton", "SnapshotTesting"], path: "SimpleButtonTests")
+    ]
 )
